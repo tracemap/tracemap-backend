8568123c44df3c64dd5d72ffab188be83b96f563
Eik-S <eikem@posteo.de>
2017-12-07 12:07:45 +0100

    New get_tweet_info api call with called function and testcase

--- api-tests/twitter/test_twitter_api.py
+++ api-tests/twitter/test_twitter_api.py
@@ -14,6 +14,19 @@ def test_get_user_info():
         if not value:
             raise TypeError("Key %s for user %s is not defined!" % (key, user_id))
 
+def test_get_tweet_info():
+    tweet_id = "938472954735755264"
+    tweets_info = get_tweet_info( tweet_id)
+    tweet_info = tweets_info[0][tweet_id]
+    """ Check if returned userInfo is a list """
+    assert type(tweets_info) is list
+    """ Check if a single tweet info dict has 8 key/value pairs """
+    assert len(tweet_info) is 8
+    """ Check if all keys have defined values """
+    for key, value in tweet_info.items():
+        if not value:
+            raise TypeError("Key %s for tweet %s is not defined!" % (key, tweet_id))
+
 def test_get_retweeters():
     tweet_id = "935572279693516800"
     followers_list = get_retweeters(tweet_id)
--- server.py
+++ server.py
@@ -10,9 +10,14 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 def twitter_get_retweeters(tweet_id):
     return jsonify(twitterApi.get_retweeters(tweet_id))
 
+@app.route('/twitter/get_tweet_info/<string:tweet_id>')
+def twitter_get_tweet_info(tweet_id):
+    return jsonify(twitterApi.get_tweet_info(tweet_id))
+
 @app.route('/twitter/get_user_info/<string:user_id>')
 def twitter_get_user_info(user_id):
     return jsonify(twitterApi.get_user_info(user_id))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
