56d9c5ea086b345c1d83b115a229eb5d5a584d92
brunopace <bruno.pace@gmail.com>
2017-12-08 19:10:35 +0100

    function get_followers tested and working, returns a dictionary that will be converted to a json at the server.py by its flask.jsonify function call

--- api/neo4j/neo4jApi.py
+++ api/neo4j/neo4jApi.py
@@ -3,7 +3,7 @@ import json
 import time
 import os
 
-def request_database(request_string):
+def __request_database(request_string):
     uri = os.environ.get('NEO4J_URI')
     driver = GraphDatabase.driver(uri, auth=(os.environ.get('NEO4J_USER'),
                                   os.environ.get('NEO4J_PASSWORD')))
@@ -11,18 +11,18 @@ def request_database(request_string):
         with session.begin_transaction() as transaction:
             return transaction.run(request_string).data()
 
-def build_lookup_dictionary(node_list):
+def __build_lookup_dictionary(node_list):
     result_dictionary = {}
     for node in node_list:
         result_dictionary.update({node.id:node.properties['uid']})
     return result_dictionary
 
 def get_followers(user_ids):
-    followers_json = {}
+    followers_dictionary = {}
     database_query = ''
     first_iteration = True
     if len(user_ids) <= 1:
-        return followers_json
+        return followers_dictionary
     for uid in user_ids:
         if first_iteration:
             database_query += 'MATCH (u:USER) WHERE u.uid = "' + uid + '" '
@@ -31,16 +31,18 @@ def get_followers(user_ids):
         database_query += 'OR u.uid = "' + uid + '" '
     database_query += 'WITH COLLECT(u) AS us UNWIND us AS u1 UNWIND us AS u2 '
     database_query += 'MATCH (u1)-[r]->(u2) RETURN COLLECT(r), us;'
-    database_response = request_database(database_query)
-    dictionary_lookup = build_lookup_dictionary(database_response[0]['us'])
+    database_response = __request_database(database_query)
+    if database_response == []:
+        return followers_dictionary
+    dictionary_lookup = __build_lookup_dictionary(database_response[0]['us'])
     for relation in database_response[0]['COLLECT(r)']:
         user = dictionary_lookup[relation.end]
         follower = dictionary_lookup[relation.start]
-        if user not in followers_json:
-            followers_json.update({user:{'followers':[follower]}})
+        if user not in followers_dictionary:
+            followers_dictionary.update({user:{'followers':[follower]}})
         else:
-            followers_json[user]['followers'].append(follower)
-    return followers_json
+            followers_dictionary[user]['followers'].append(follower)
+    return followers_dictionary
 
 def add_user_info(user_info):
     pass
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 import api.twitter.twitterApi as twitterApi
+import api.neo4j.neo4jApi as neo4jApi
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -18,6 +19,9 @@ def twitter_get_tweet_info(tweet_id):
 def twitter_get_user_info(user_id):
     return jsonify(twitterApi.get_user_info(user_id))
 
+@app.route('/neo4j/get_followers/<string:user_ids>')
+def neo4j_get_followers(user_ids):
+    return jsonify(neo4jApi.get_followers(user_ids.split(",")))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
