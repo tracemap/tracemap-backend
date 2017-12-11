4ddb46cfe488c44fdb43e6d87b550acce2ff1638
brunopace <bruno.pace@gmail.com>
2017-12-11 19:58:12 +0100

    First version of the functions add_user_info and get_user_info completed and tested.

--- api-tests/neo4j/test_neo4j_api.py
+++ api-tests/neo4j/test_neo4j_api.py
@@ -29,3 +29,34 @@ def test_get_followers():
             """whose elements are also inside the retweeters list"""
             assert follower in retweeters
 
+def test_add_user_info():
+    """Example of users already in the database for the test to work!!!"""
+    example_users = ['8557', '56', '560306494', '625025485']
+    """Use this function to get public info from Twitter"""
+    users_info = twitter.get_user_info(example_users)
+    """Guarantee that the data is in a dictionary..."""
+    assert type(users_info) is dict
+    """...with a 'response' key."""
+    assert 'response' in users_info
+    for uid in users_info['response'].keys():
+        """Check if each user id contains a dictionary"""
+        assert type(users_info['response'][uid]) is dict
+
+    """Call the function add_user_info() to write to the database"""
+    success = neo4j.add_user_info(users_info)
+    """This test does not check if the info was written in the database."""
+    assert success
+    
+def test_get_user_info():
+    """Example of a user already in the database"""
+    user_id = "8557"
+    user_info = neo4j.get_user_info(user_id)
+    properties = ["timestamp","name","screen_name","location","lang",
+                  "followers_count","friends_count","statuses_count",
+                  "created_at","profile_img_url","uid"]
+    """Guarantee that the info is a dictionary"""
+    assert type(user_info) is dict
+    """Guarantee that 'response' is in the dictionary"""
+    assert 'response'in user_info
+    for property_name in user_info['response'][user_id]:
+        assert property_name in properties
